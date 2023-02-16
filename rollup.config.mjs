24b31424d18c75430b8b4f87a033c690cd5adfb4
captain-woof <captainwoof.official@gmail.com>
2023-02-16 22:45:08 +0530

    Popup bundling config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,11 +3,14 @@ import typescript from "@rollup/plugin-typescript";
 import terser from "@rollup/plugin-terser";
 import resolve from "@rollup/plugin-node-resolve";
 import del from "rollup-plugin-delete";
-import copy from 'rollup-plugin-copy';
-import json from "@rollup/plugin-json";
+import copy from "rollup-plugin-copy";
+import vue from "@vitejs/plugin-vue";
+import babel from "@rollup/plugin-babel";
+import replace from "@rollup/plugin-replace";
+import html from "rollup-plugin-generate-html-template";
+import styles from "rollup-plugin-styles";
 
 export default defineConfig([
-
 
     // For service-worker + essentials
     {
@@ -16,7 +19,7 @@ export default defineConfig([
         output: {
             file: "dist/service_worker.js",
             format: "es",
-            sourcemap: process.env.BUILD_ENV === "development"
+            sourcemap: isDev
         },
         plugins: [
             del({
@@ -43,5 +46,56 @@ export default defineConfig([
                 ]
             })
         ]
+    },
+
+    // For Vue app for pop-up
+    {
+        input: "src/popup/index.ts",
+        cache: true,
+        output: {
+            format: "esm",
+            file: "dist/app.js",
+            sourcemap: isDev,
+            assetFileNames: "[name][extname]",
+        },
+        watch: {
+            buildDelay: 250,
+            clearScreen: true
+        },
+        plugins: [
+            resolve({
+                browser: true,
+                extensions: [".js", ".ts", ".vue"],
+            }),
+            vue({
+                isProduction: !isDev,
+                style: {
+                    trim: !isDev
+                }
+            }),
+            babel({
+                babelHelpers: "bundled",
+                extensions: [".js", ".ts", ".vue"],
+                exclude: ["**/*.scss", "**/*.css"]
+            }),
+            styles({
+                minimize: !isDev,
+                mode: ["extract", "styles.css"]
+            }),
+            replace({
+                preventAssignment: true,
+                values: {
+                    'process.env.NODE_ENV': JSON.stringify(process.env.BUILD_ENV)
+                }
+            }),
+            (isDev && terser()),
+            html({
+                template: 'src/popup/template.html',
+                target: 'dist/popup.html',
+            }),
+        ]
     }
 ]);
+
+// HELPERS
+var isDev = process.env.BUILD_ENV === "development";
---
+++ src/popup/App.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import "./styles/reset.css";
+</script>
+
+<template>
+    <h1>CHIRAG</h1>
+</template>
+
+<styles lang="scss">
+    h1 {
+        color: blue;
+    }
+</styles>

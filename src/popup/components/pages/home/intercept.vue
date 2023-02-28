9eb2766d5bc5da230ac52854e84c81bcf5608883
captain-woof <captainwoof.official@gmail.com>
2023-02-28 21:29:30 +0530

    Intercept delete functionality

--- src/popup/components/pages/home/intercept.vue
+++ src/popup/components/pages/home/intercept.vue
@@ -14,17 +14,22 @@ const router = useRouter();
 const store = useChiragStore();
 
 // Methods
-const handleInterceptClick = () => {
-    router.push({ name: "editor", params: { interceptUrl: intercept.interceptUrl } });
+const handleInterceptClick = async () => {
+    await router.push({ name: "editor", params: { interceptUrl: intercept.interceptUrl } });
+}
+
+const handleInterceptDelete = async () => {
+    await store.removeIntercept(intercept.interceptUrl);
 }
 </script>
 
 <template>
-    <v-list-item append-icon="mdi-chevron-right" @click="handleInterceptClick" class="intercept"
-        :class="{ 'intercept--disabled': !intercept.enabled }" v-ripple>
+    <v-list-item @click="handleInterceptClick" class="intercept" :class="{ 'intercept--disabled': !intercept.enabled }"
+        v-ripple>
         <!-- Checkbox - enable/disable -->
         <template #prepend>
-            <v-checkbox color="primary" :model-value="intercept.enabled" @click.stop="store.enableDisableIntercept(intercept.interceptUrl)"></v-checkbox>
+            <v-checkbox color="primary" :model-value="intercept.enabled"
+                @click.stop="store.enableDisableIntercept(intercept.interceptUrl)"></v-checkbox>
         </template>
 
         <!-- Title -->
@@ -43,6 +48,11 @@ const handleInterceptClick = () => {
                     </h2>
                 </template>
             </v-tooltip>
+        </template>
+
+        <!-- Delete intercept -->
+        <template #append>
+            <v-btn icon="mdi-close" @click.stop="handleInterceptDelete" variant="flat"></v-btn>
         </template>
     </v-list-item>
 </template>
